57e410cb5187c01ec774e6808ac515eefb580a0f
Amodh Dhakal <adhakal2@ncsu.edu>
2024-12-13 21:55:55 -0500

    Add some documentation to main file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
-void main() {
-    System.out.println("Chess setup successful!");
+/**
+ * The main function of this chess program. This is the function that's called when
+ * the user wants to play chess in the terminal
+ */
+void main()
+{
+    System.out.println( "Chess setup successful!" );
 }
